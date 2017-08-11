e3712fce81d0de7b815bc7e3b256e912b2108547
Mag2017 <Magbor.Atem@gmail.com>
2017-08-10 23:57:43 -0400

    cards

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+var inquirer = require('inquirer');
+    var BasicCard = function(front, back) {
+        this.front =front;
+        this.back = back;
+                    console.log("front","back")
+        }
+    module.exports = BasicCard;
+
